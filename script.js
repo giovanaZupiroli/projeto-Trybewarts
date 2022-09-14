05c3cf05f10da8ea7931057d92d101dac9364226
GiovanaZ <giovanazupiroli@gmail.com>
2022-09-14 15:38:01 -0300

    Requisitos 3 e 4 OK

---
+++ script.js
@@ -0,0 +1,13 @@
+const button = document.querySelector('#btn');
+const email = document.querySelector('#email');
+const senha = document.querySelector('#senha');
+
+function alertaBotao () {
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+
+button.addEventListener('click', alertaBotao);
