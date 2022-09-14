604bfcc4246a055546688ac203d08c44955fca56
Nathalia Marana <nathalia_marana@hotmail.com>
2022-09-14 17:28:02 -0300

    Requisito 17 e 18 OK

--- script.js
+++ script.js
@@ -6,8 +6,17 @@ function alertaBotao() {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
     alert('Olá, Tryber!');
   } else {
-    alert('Email ou senha inválidos.');
+    alert('mail ou senha inválidos.');
   }
 }
 
 button.addEventListener('click', alertaBotao);
+
+function habilitaEnviar() {
+  const submitBtn = document.getElementById('submit-btn');
+  const agreement = document.getElementById('agreement');
+  agreement.addEventListener('change', () => {
+    submitBtn.disabled = !agreement.checked;
+  });
+}
+habilitaEnviar();
